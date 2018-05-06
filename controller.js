7ebff383b0140099b4e4b4cba99c5733c0fd068c
cat2151 <fct_2004@mail.goo.ne.jp>
2018-05-06 20:08:03 +0900

    演奏時のSiOPM.stop()が失敗した場合に、Sionic.js で鳴らせるようにした

--- controller.js
+++ controller.js
@@ -59,7 +59,7 @@ function($scope, $location, $timeout, GeneratorService) {
     try{
       SIOPM.stop();
     }catch(e){
-      console.log(e);
+      $scope.SIOPM_fallback();
     }
     Pico.pause();
 
@@ -108,15 +108,18 @@ function($scope, $location, $timeout, GeneratorService) {
     SIOPM.play();
   };
 
+  $scope.SIOPM_fallback = function() {
+    $scope.mmlFormat = "sionic";
+  };
+
   try{
     SIOPM.initialize(); // [前提] SIOPMのプロパティへ各functionを代入し終わっていること
   }catch(e){
-    //fallback
-    $scope.mmlFormat = "sionic";
+    $scope.SIOPM_fallback();
   }
+
   $timeout(function() {
     setParamsToScopeFromUrl(); // [前提] $scopeのプロパティへ各functionを代入し終わっていること
   }, 0);
-
 
 }]);
